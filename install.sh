eed2c550eaf615d70076c68aff18b4626419e5d0
hotvic <victor.feradecs@gmail.com>
2012-10-27 12:26:34 -0300

    Added installation script(install.sh)

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Clonning extension..."
+git clone git://github.com/hotvic/Alsa-Mixer.git Alsa-Mixer
+echo "Creating extension folder..."
+mkdir -p ~/.local/share/gnome-shell/extensions/Alsa-Mixer\@so-dicas.info
+echo "Installing extension..."
+cp Alsa-Mixer/extension.js ~/.local/share/gnome-shell/extensions/Alsa-Mixer\@so-dicas.info/
+cp Alsa-Mixer/metadata.json ~/.local/share/gnome-shell/extensions/Alsa-Mixer\@so-dicas.info/
+echo "Cleaning..."
+rm -rf Alsa-Mixer/
+echo "Done!"
