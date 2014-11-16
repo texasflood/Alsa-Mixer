8c75d6762b2aa5f622b957142f93d25d3ca23b01
Anas Syed <anas27@gmail.com>
2014-11-16 05:20:04 +0000

    Added mute button and improved responsiveness

--- extension.js
+++ extension.js
@@ -34,155 +34,172 @@ const VOLUME_STEP = 4;
 const MIXER_ELEMENT = 'Master';
 
 const AlsaMixer = new Lang.Class({
-    Name: 'AlsaMixer',
-    Extends: PanelMenu.SystemStatusButton,
-    
-    _init: function() {
-        this.parent('audio-volume-medium', _('Volume'));
-        
-        this.statusIcon = new St.Icon({
-            icon_name: 'audio-volume-medium',
-            style_class: 'status-icon'
-        });
-        this.actor.add_actor(this.statusIcon);
-        
-        this._onScrollId = this.actor.connect('scroll-event',
-            Lang.bind(this, this._onScroll));
-        
-        this._cVolume = this._getVolume();
-        this._muted = this._cVolume < 1 ? true : false;
-        this._updateIcon(this._cVolume);
-        
-        this.pup = new PopupMenu.PopupSliderMenuItem(this._cVolume / 64);
-        this._onSliderId = this.pup.connect('value-changed',
-                Lang.bind(this, this._onSlider));
-        this.menu.addMenuItem(this.pup);
-        
-        this._timeoutId = Mainloop.timeout_add_seconds(1,
-                Lang.bind(this, this._onUpdate));
-    },
-    
-    _getVolume: function() {
-        let cmd = GLib.spawn_command_line_sync(
-            'env LANG=C amixer get %s'.format(MIXER_ELEMENT));
-        var op = String(cmd[1]);
-        op = (op.slice(-3))[0];
-        var muted = (op === "f");
-        if (muted)
-        {
-          return 0;
-        }
-        let re = /\[(\d{1,3})\%\]/m;
-        let values = re.exec(cmd[1]);
-        return Math.round((values[1]*(64.0/100.0)));
-    },
+  Name: 'AlsaMixer',
+  Extends: PanelMenu.SystemStatusButton,
 
-    _getMute: function() {
-        let cmd = GLib.spawn_command_line_sync(
-            'env LANG=C amixer get %s'.format(MIXER_ELEMENT));
-        var op = String(cmd[1]);
-        op = (op.slice(-3))[0];
-        var muted = (op === "f");
-        return muted;
-    },
-    
-    _setVolume: function(value) {
-        let cmd = GLib.spawn_command_line_async(
-                'amixer -q set %s %s %%'.format(MIXER_ELEMENT, value));
-        
-        this._cVolume = value;
-        
-        this._updateIcon(value);
-    },
-    
-    _updateIcon: function(value) {
-        if (this.statusIcon.get_icon_name() != this._getIcon(value)) {
-            let icon = this._getIcon(value);
-            this.statusIcon.set_icon_name(icon);
-            this.setIcon(icon);
-        }
-    },
-    
-    _getIcon: function(volume) {
-        let rvalue = 'audio-volume-muted';
-        if (volume < 1) {
-            rvalue = 'audio-volume-muted';
-        } else {
-            let num = Math.floor(3 * volume / 64) + 1;
+  _init: function() {
+    this.parent('audio-volume-medium', _('Volume'));
 
-            if (num >= 3)
-                rvalue = 'audio-volume-high';
-            else if(num < 2)
-                rvalue = 'audio-volume-low';
-            else
-                rvalue = 'audio-volume-medium';
-        }
-        return rvalue;
-    },
-    
-    _onScroll: function(widget, event) {
-        
-        let di = event.get_scroll_direction();
-        
-        if (di == Clutter.ScrollDirection.DOWN
-                && Number(this._cVolume) > VOLUME_STEP) {
-            this._setVolume(Number(this._cVolume) - VOLUME_STEP);
-        } else if (di == Clutter.ScrollDirection.DOWN
-                && Number(this._cVolume) <= VOLUME_STEP) {
-            this._setVolume(0);
-        } else if(di == Clutter.ScrollDirection.UP
-                && Number(this._cVolume) < 64 - VOLUME_STEP) {
-            this._setVolume(Number(this._cVolume) + VOLUME_STEP);
-        } else if(di == Clutter.ScrollDirection.UP
-                && Number(this._cVolume) >= 64 - VOLUME_STEP) {
-            this._setVolume(64);
-        }
-        if (this._getMute() && this._cVolume != 0)
-        {
-          GLib.spawn_command_line_async(
-              'amixer -q set %s unmute '.format(MIXER_ELEMENT, value));
-        }
-        this.pup.setValue(Number(this._cVolume) / 64);
-    },
-    
-    _onSlider: function(slider, value) {
-      let volume = Math.round(value * 64);
-      this.pup.setValue(volume/64.0);
-      if (this._getMute() && value != 0)
-      {
-        GLib.spawn_command_line_async(
-            'amixer -q set %s unmute '.format(MIXER_ELEMENT, value));
-      }
-      this._setVolume(volume);
-    },
-    
-    _onUpdate: function() {
-        this._cVolume = this._getVolume();
-        this._updateIcon(this._cVolume);
-        this.pup.setValue(Number(this._cVolume) / 64);
-        return true;
-    },
-    
-    destroy: function() {
-        this.parent();
-        Mainloop.source_remove(this._timeoutId);
-        this.actor.disconnect(this._onScrollId);
-        this.pup.disconnect(this._onSliderId);
-    },
+    this.statusIcon = new St.Icon({
+      icon_name: 'audio-volume-medium',
+      style_class: 'status-icon'
+    });
+    this.actor.add_actor(this.statusIcon);
+
+    this._onScrollId = this.actor.connect('scroll-event',
+        Lang.bind(this, this._onScroll));
+
+    this._cVolume = this._getVolume();
+    this._muted = this._getMute();
+    this._updateIcon(this._cVolume, this._muted);
+
+    this.muteMenuItem = new PopupMenu.PopupSwitchMenuItem(_("Sound"), !this._muted, { reactive: true });
+    this.muteMenuItem.connect('toggled', Lang.bind(this, this._handleMuteMenuItem));
+    this.menu.addMenuItem(this.muteMenuItem);
+
+    this.pup = new PopupMenu.PopupSliderMenuItem(this._cVolume / 64);
+    this._onSliderId = this.pup.connect('value-changed',
+        Lang.bind(this, this._onSlider));
+    this.menu.addMenuItem(this.pup);
+
+    this._timeoutId = Mainloop.timeout_add_seconds(1,
+        Lang.bind(this, this._onUpdate));
+  },
+
+  _handleMuteMenuItem: function(actor, event) {
+    if (event == false)
+    {
+      GLib.spawn_command_line_async(
+          'amixer -q set %s mute '.format(MIXER_ELEMENT));
+    }
+    else if (event == true)
+    {
+      GLib.spawn_command_line_async(
+          'amixer -q set %s unmute '.format(MIXER_ELEMENT));
+    }
+    this._onUpdate();
+  }, 
+
+  _getVolume: function() {
+    let cmd = GLib.spawn_command_line_sync(
+        'env LANG=C amixer get %s'.format(MIXER_ELEMENT));
+    let re = /\[(\d{1,3})\%\]/m;
+    let values = re.exec(cmd[1]);
+    return Math.round((values[1]*(64.0/100.0)));
+  },
+
+  _getMute: function() {
+    let cmd = GLib.spawn_command_line_sync(
+        'env LANG=C amixer get %s'.format(MIXER_ELEMENT));
+    var op = String(cmd[1]);
+    op = (op.slice(-3))[0];
+    var muted = (op === "f");
+    return muted;
+  },
+
+  _setVolume: function(value) {
+    let cmd = GLib.spawn_command_line_async(
+        'amixer -q set %s %s %%'.format(MIXER_ELEMENT, value));
+
+    this._cVolume = value;
+    var muted = this._getMute();
+    if (value != 0 || muted)
+    {
+      GLib.spawn_command_line_async(
+          'amixer -q set %s unmute '.format(MIXER_ELEMENT));
+      muted = false;
+    }
+
+    this._updateIcon(value, false);
+  },
+
+  _updateIcon: function(value, muted) {
+    var iconValue = this._getIcon(value, muted);
+    if (this.statusIcon.get_icon_name() != iconValue) {
+      let icon = iconValue;
+      this.statusIcon.set_icon_name(icon);
+      this.setIcon(icon);
+    }
+  },
+
+  _getIcon: function(volume, muted) {
+    let rvalue = 'audio-volume-muted';
+    if (volume < 1 || muted) {
+      rvalue = 'audio-volume-muted';
+    } else {
+      let num = Math.floor(3 * volume / 64) + 1;
+
+      if (num >= 3)
+        rvalue = 'audio-volume-high';
+      else if(num < 2)
+        rvalue = 'audio-volume-low';
+      else
+        rvalue = 'audio-volume-medium';
+    }
+    return rvalue;
+  },
+
+  _onScroll: function(widget, event) {
+
+    let di = event.get_scroll_direction();
+
+    if (di == Clutter.ScrollDirection.DOWN
+        && Number(this._cVolume) > VOLUME_STEP) {
+      this._setVolume(Number(this._cVolume) - VOLUME_STEP);
+    } else if (di == Clutter.ScrollDirection.DOWN
+        && Number(this._cVolume) <= VOLUME_STEP) {
+      this._setVolume(0);
+    } else if(di == Clutter.ScrollDirection.UP
+        && Number(this._cVolume) < 64 - VOLUME_STEP) {
+      this._setVolume(Number(this._cVolume) + VOLUME_STEP);
+    } else if(di == Clutter.ScrollDirection.UP
+        && Number(this._cVolume) >= 64 - VOLUME_STEP) {
+      this._setVolume(64);
+    }
+    if (this._getMute() && this._cVolume != 0)
+    {
+      this.muteMenuItem.setToggleState(false);
+    }
+    this.pup.setValue(Number(this._cVolume) / 64);
+    this._onUpdate();
+  },
+
+  _onSlider: function(slider, value) {
+    let volume = Math.round(value * 64);
+    this.pup.setValue(volume/64.0);
+    this._setVolume(volume);
+    this._onUpdate();
+  },
+
+  _onUpdate: function() {
+    this._cVolume = this._getVolume();
+    this._muted = this._getMute();
+    this._updateIcon(this._cVolume, this._muted);
+    this.pup.setValue(Number(this._cVolume) / 64);
+    this.muteMenuItem.setToggleState(!this._muted);
+    return true;
+  },
+
+  destroy: function() {
+    this.parent();
+    Mainloop.source_remove(this._timeoutId);
+    this.actor.disconnect(this._onScrollId);
+    this.pup.disconnect(this._onSliderId);
+  },
 });
 
 function init() {
-    // pass
+  // pass
 }
 
 var AM;
 
 function enable() {
-    AM = new AlsaMixer();
-    Main.panel.addToStatusArea('AlsaMixer', AM);
+  AM = new AlsaMixer();
+  Main.panel.addToStatusArea('AlsaMixer', AM);
 }
 
 function disable() {
-    AM.destroy();
-    AM = null;
+  AM.destroy();
+  AM = null;
 }
